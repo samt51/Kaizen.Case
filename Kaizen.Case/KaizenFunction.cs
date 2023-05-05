c73af8805c9e0f7452c3d99f00f70fcc089a2d62
sametbaglan <87297893+sametbaglan@users.noreply.github.com>
2023-05-05 11:31:19 +0300

    Add project files.

---
+++ Kaizen.Case/KaizenFunction.cs
@@ -0,0 +1,49 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Kaizen.Case
+{
+    public static class KaizenFunction
+    {
+        public static string JsonConvertToCsv(string json)
+        {
+            var config = new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
+            {
+                Delimiter = ",",
+                HasHeaderRecord = true,
+            };
+            JArray jArray = JArray.Parse(json);
+
+            using (StringWriter writer = new StringWriter())
+            {
+                // CsvWriter nesnesi oluştur
+                using (CsvWriter csv = new CsvWriter(writer, config))
+                {
+                    // Header satırı için özellik isimlerini yaz
+                    foreach (JProperty property in jArray.First.Children())
+                    {
+                        csv.WriteField(property.Name);
+                    }
+                    csv.NextRecord();
+
+                    // Her öğe için satır yaz
+                    foreach (JObject obj in jArray)
+                    {
+                        foreach (JProperty property in obj.Children())
+                        {
+                            csv.WriteField(property.Value.ToString());
+                        }
+                        csv.NextRecord();
+                    }
+                }
+
+                // CSV dosyasını yaz
+
+                return writer.ToString();
+            }
+           
+        }
+    }
+}
---
+++ Kaizen.Case/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace Kaizen.Case
+{
+    class Program
+    {
+        static readonly string CharPool = "ACDEFGHKLMNPRTXYZ234579";
+        static readonly int CodeLength = 8;
+
+
+
+        static void Main(string[] args)
+        {
+            // Kodların karakter kümesi
+            string characters = "ACDEFGHKLMNPRTXYZ234579";
+
+            // Rastgele sayı üretmek için kullanılacak nesne
+            Random random = new Random();
+
+            // Kod uzunluğu
+            int length = 8;
+
+            // Kod sayısı
+            int count = 100;
+
+            // Kodları saklamak için kullanılacak dizi
+            string[] codes = new string[count];
+
+            // Kod üretimi
+            for (int i = 0; i < count; i++)
+            {
+                StringBuilder codeBuilder = new StringBuilder();
+
+                // Kodun her bir karakteri rastgele seçilir
+                for (int j = 0; j < length; j++)
+                {
+                    char c = characters[random.Next(characters.Length)];
+                    codeBuilder.Append(c);
+                }
+
+                string code = codeBuilder.ToString();
+
+                // Kod daha önce oluşturulmuşsa, tekrar oluşturulur
+                while (Array.IndexOf(codes, code) >= 0)
+                {
+                    codeBuilder.Clear();
+                    for (int j = 0; j < length; j++)
+                    {
+                        char c = characters[random.Next(characters.Length)];
+                        codeBuilder.Append(c);
+                    }
+                    code = codeBuilder.ToString();
+                }
+
+                codes[i] = code;
+            }
+
+            // Oluşturulan kodlar yazdırılır
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(codes[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+ 
+
+
+
+}
+
